38ed5c925e59edee7e901939a6ef70a6d0be2d44
exert777 <alter0910@mail.ru>
2024-11-29 15:52:10 +0300

    отрефакторит логику выбора разметки исходя из статуса пользователя

--- src/components/header/header-nav-user.tsx
+++ src/components/header/header-nav-user.tsx
@@ -5,44 +5,43 @@ type HeaderNavUserProps ={
   authorizationStatus: AuthorizationStatus;
 }
 
-const isLoginUser = (status: AuthorizationStatus[keyof AuthorizationStatus]): JSX.Element => {
-  if(status === AuthorizationStatus.Auth){
-    return (
-      <>
-        <li className="header__nav-item user">
-          <a
-            className="header__nav-link header__nav-link--profile"
-            href="#"
-          >
-            <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-            <span className="header__user-name user__name">
-              Oliver.conner@gmail.com
-            </span>
-            <span className="header__favorite-count">3</span>
-          </a>
-        </li>
-        <li className="header__nav-item">
-          <a className="header__nav-link" href="#">
-            <span className="header__signout">Sign out</span>
-          </a>
-        </li>
-      </>
-    );
-  }
+const isLoginUser = (status: AuthorizationStatus[keyof AuthorizationStatus]): boolean => status === AuthorizationStatus.Auth;
 
-  return (
-    <li className="header__nav-item">
-      <Link to={AppRoute.Login} className="header__nav-link">
-        <span className="header__login">Sign in</span>
-      </Link>
+
+const getMarkupForAuth = (): JSX.Element => (
+  <>
+    <li className="header__nav-item user">
+      <a
+        className="header__nav-link header__nav-link--profile"
+        href="#"
+      >
+        <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+        <span className="header__user-name user__name">
+          Oliver.conner@gmail.com
+        </span>
+        <span className="header__favorite-count">3</span>
+      </a>
     </li>
-  );
-};
+    <li className="header__nav-item">
+      <a className="header__nav-link" href="#">
+        <span className="header__signout">Sign out</span>
+      </a>
+    </li>
+  </>
+);
+
+const getMarkupForNoAuth = (): JSX.Element => (
+  <li className="header__nav-item">
+    <Link to={AppRoute.Login} className="header__nav-link">
+      <span className="header__login">Sign in</span>
+    </Link>
+  </li>
+)
 
 function HeaderNavUser({authorizationStatus}: HeaderNavUserProps) {
   return (
     <ul className="header__nav-list">
-      {isLoginUser(authorizationStatus)}
+      {isLoginUser(authorizationStatus) ? getMarkupForAuth() : getMarkupForNoAuth()}
     </ul>
   );
 }
